28577a1747b930d855a783e2f8d2e0abcd1a8ac4
Philip Reich <philip.a.reich@gmail.com>
2025-11-17 07:47:06 +0100

    CORS & Headers

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -1,9 +1,18 @@
 // src/hooks.server.ts
+import type { Handle } from '@sveltejs/kit';
 import { initializeDatabaseAndSetCache, refreshData } from '$lib/server/database.server';
 import { scheduleJob } from 'node-schedule';
 
 initializeDatabaseAndSetCache();
 
 const job = scheduleJob('38 1 * * *', async function () {
-    refreshData();
+	refreshData();
 });
+
+export const handle: Handle = async ({ event, resolve }) => {
+	const response = await resolve(event);
+	response.headers.set('Cross-Origin-Opener-Policy', 'same-origin');
+	response.headers.set('Cross-Origin-Embedder-Policy', 'require-corp');
+	response.headers.set('Cross-Origin-Resource-Policy', 'same-origin');
+	return response;
+};
